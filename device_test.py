0ee2cf6abc3d82a21ed833177735efb13885c486
Ketan <ketanpalankar123@gmail.com>
2024-12-05 02:25:13 +0530

    Updating test script to check device compatibility

--- device_test.py
+++ device_test.py
@@ -6,20 +6,11 @@ from streamlit_webrtc import WebRtcMode, webrtc_streamer
 import logging
 import os
 
-frame_rate = 5
 webrtc_streamer(
     key="media-constraints",
     mode=WebRtcMode.SENDRECV,
     rtc_configuration={  # Add this config
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    },
-    media_stream_constraints={
-        "video": {"frameRate": {"ideal": frame_rate}},
-    },
-    video_html_attrs={
-        "style": {"width": "50%", "margin": "0 auto", "border": "5px yellow solid"},
-        "controls": False,
-        "autoPlay": True,
-    },
+    }
 )
-st.write(f"The frame rate is set as {frame_rate}. Video style is changed.")
+st.write(f"WIP")
